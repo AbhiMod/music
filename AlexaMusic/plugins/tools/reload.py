30ade877c19de1fafee8b6551dbcd862d918c61d
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-19 22:31:47 +0530

    Update reload.py

--- AlexaMusic/plugins/tools/reload.py
+++ AlexaMusic/plugins/tools/reload.py
@@ -86,17 +86,11 @@ async def close_menu(_, CallbackQuery):
     try:
         await CallbackQuery.message.delete()
         await CallbackQuery.answer()
-    except:
-        return
-
-
-@app.on_callback_query(filters.regex("close") & ~BANNED_USERS)
-async def close_menu(_, CallbackQuery):
-    try:
-        await CallbackQuery.message.delete()
-        await CallbackQuery.answer()
-    except:
-        return
+        await CallbackQuery.message.reply_text(
+            f"𝓒𝓵𝓸𝓼𝓮𝓭 𝓑𝔂 ➣ {CallbackQuery.from_user.mention}"
+        )
+    except Exception as e:
+        print(e)
 
 
 @app.on_callback_query(filters.regex("stop_downloading") & ~BANNED_USERS)
