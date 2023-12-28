4a368753633473b339bb93e085abaf6065acb1d2
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-28 11:23:49 +0530

    Update and rename am.py to am

--- am.py
+++
@@ -1,4 +0,0 @@
-from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf music && git clone https://github.com/AbhiMod/music && cd music && python3 -m AlexaMusic")
-# try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
